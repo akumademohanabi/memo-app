5d2ee87bef5850d3222cf53ef9fc4c080b88ab4d
F.RN1945 <r.furukawa.sys23@morijyobi.ac.jp>
2024-05-29 11:35:51 +0900

    memo-app

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const { Extension } = require("typescript");
+
+module.exports = {
+    // ビルドした際の出力先
+    output: {
+        path: `${__dirname}/dist`,
+        filename: "bundle.js"
+    },
+    // モード指定（development,production）
+    mode: "development",
+    // import時のpass指定で拡張子を省略する
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    // 開発用サーバーで起動するパス
+    devServer: {
+        static: {
+            directory: `${__dirname}/dist`,
+        },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                loader: "ts-loader",
+            },
+        ],
+    }
+}
